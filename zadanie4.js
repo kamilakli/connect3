33de4170995541778559e2ecbab17e4f9c418ef8
Kamila Klimkowicz <kamila.klimkowicz1@gmail.com>
2019-06-11 19:11:12 +0200

    Zadanie 4 bez inputa

---
+++ zadanie4.js
@@ -0,0 +1,18 @@
+let circleColor = "#bf3eff";
+function changeColor(currentColor) {
+    console.log("what");
+    if(currentColor === "#bf3eff"){
+        return "#fff68f";
+    } else if (currentColor === "#fff68f")
+    {
+        return "#a8251d";
+    } else {
+        return "#bf3eff";
+    }
+}
+
+// function that displays value of input
+function showColor () {
+    circleColor = changeColor(circleColor);
+    document.getElementById("colorChanger").style.backgroundColor = circleColor;} 
+
