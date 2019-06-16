45501fccafce9daa220740c5314a8c6190f82c5b
Kamila Klimkowicz <kamila.klimkowicz1@gmail.com>
2019-06-16 17:07:02 +0200

    zadanie 2

---
+++ zad2.js
@@ -0,0 +1,15 @@
+const input1 = () => document.getElementById("inputOne").value; 
+const input2 = () => document.getElementById("inputTwo").value;
+const input3 = () => document.getElementById("inputThree").value;
+
+function showresult () {
+    let firstInput =Number(input1());
+    let secondInput=Number(input2());
+    let thirdInput=Number(input3());
+displayInput (firstInput, secondInput, thirdInput);
+}
+
+const displayInput = (in1, in2, in3) =>{
+let sum = in1 + in2 + in3;
+return document.getElementById("displayresult").innerHTML = sum;
+}
