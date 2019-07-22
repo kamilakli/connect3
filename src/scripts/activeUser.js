ccfee8825bc20b5bef54e31493aa4c63e723b19f
Kamila Klimkowicz <kamila.klimkowicz1@gmail.com>
2019-07-22 10:46:30 +0200

    magda spr2

--- src/scripts/activeUser.js
+++ src/scripts/activeUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {DisplayUserButton} from './displayUserButton';
 
+//Tu mój aktywny user kod, chciałam przenieść button do oddzielnego komponentu, ale coś poszło nie tak, dlatego import na górze
 export class ActiveUser extends React.Component {
     constructor(props) {
         super(props);
